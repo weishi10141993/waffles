903ab40cba7e0f5a3633b2af5aa8582b49c97662
LauPM <laurapermol@gmail.com>
2024-05-21 10:09:15 +0200

    progress bar :)

--- src/waffles/extractor.py
+++ src/waffles/extractor.py
@@ -5,8 +5,9 @@ import detdataformats
 from daqdataformats import FragmentType
 from rawdatautils.unpack.daphne import *
 
-import numpy as np
 import click
+import numpy as np
+from rich.progress  import track
 from uproot import recreate as rc
 
 def extract_fragment_info(frag):
@@ -30,48 +31,57 @@ def extract_fragment_info(frag):
 @click.option("--run", '-r', default = None, help="Insert the run number, ex: 026102")
 
 def main(run):
-    det         = 'HD_PDS'
-    run_path    = f'/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles/rucio_paths/{run}.txt'
-    root_file   = rc(f'/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles/root_files/{run}.root')
+    ## Check if the run number is provided ##
+    if run is None: 
+        print("\033[35mPlease provide a run(s) number(s) to be analysed, separated by commas:)\033[0m")
+        run = [int(input("Run(s) number(s): "))]
+    if len(run)!=1: runs_list = list(map(int, list(run.split(","))))
+    else: runs_list = run
 
-    with open(f'{run_path}', "r") as run_list:
-        run_paths = run_list.readlines()
-    files = [run_path.rstrip('\n') for run_path in run_paths]
-    
-    for file in files:
-        print(f'Reading {file}...')
-        h5_file   = HDF5RawDataFile(file)
-        records   = h5_file.get_all_record_ids()
-        tr_ref    = None
+    for run in runs_list:
+        run = str(run).zfill(6) # check if run have 6 digits and fill with zeros if not
+        det         = 'HD_PDS'
+        run_path    = f'/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles/rucio_paths/{run}.txt'
+        root_file   = rc(f'/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles/root_files/{run}.root')
+
+        with open(f'{run_path}', "r") as run_list:
+            run_paths = run_list.readlines()
+        files = [run_path.rstrip('\n') for run_path in run_paths]
         
-        # Iterate through records        
-        for r in records:
-            pds_geo_ids = list(h5_file.get_geo_ids_for_subdetector(r, detdataformats.DetID.string_to_subdetector(det)))
+        for raw_file in files:
+            print(f'Reading {raw_file}...')
+            h5_file   = HDF5RawDataFile(raw_file)
+            records   = h5_file.get_all_record_ids()
+            tr_ref    = None
             
-            # Iterate through geo_ids
-            for gid in pds_geo_ids:
-                frag = h5_file.get_frag(r, gid)
-                tr_header = frag.get_header().trigger_timestamp
+            # Iterate through records 
+            for r in track(records, description=f'Dumping {raw_file.split("/")[-1]} ...'):     
+                pds_geo_ids = list(h5_file.get_geo_ids_for_subdetector(r, detdataformats.DetID.string_to_subdetector(det)))
+                
+                # Iterate through geo_ids
+                for gid in pds_geo_ids:
+                    frag = h5_file.get_frag(r, gid)
+                    tr_header = frag.get_header().trigger_timestamp
 
-                if hex(gid)[3] == '0': endpoint = hex(gid)[2]
-                else                 : endpoint = hex(gid)[2:4]
+                    if hex(gid)[3] == '0': endpoint = hex(gid)[2]
+                    else                 : endpoint = hex(gid)[2:4]
 
-                # Filtering data with different timestamp on the header
-                if tr_header != tr_ref:
-        
-                    trigger, frag_id, channels, adcs, timestamps = extract_fragment_info(frag)
-                    channels = 100*int(endpoint) + channels   
+                    # Filtering data with different timestamp on the header
+                    if tr_header != tr_ref:
+            
+                        trigger, frag_id, channels, adcs, timestamps = extract_fragment_info(frag)
+                        channels = 100*int(endpoint) + channels   
 
-                    if trigger == 'full_stream': adcs = adcs.transpose()
-                   
-                    if tr_ref is None:
-                        root_file[f'waveform_primitive']       = ({'channel': channels,'adcs': np.array(adcs)})
-                        root_file[f'timestamps']               = ({'timestamps': timestamps})
-                    else:
-                        root_file[f'waveform_primitive'].extend({'channel': channels,'adcs': np.array(adcs)})
-                        root_file[f'timestamps'].extend ({'timestamps': timestamps})            
-                           
-                tr_ref = tr_header
+                        if trigger == 'full_stream': adcs = adcs.transpose()
+                    
+                        if tr_ref is None:
+                            root_file[f'waveform_primitive']       = ({'channel': channels,'adcs': np.array(adcs)})
+                            root_file[f'timestamps']               = ({'timestamps': timestamps})
+                        else:
+                            root_file[f'waveform_primitive'].extend({'channel': channels,'adcs': np.array(adcs)})
+                            root_file[f'timestamps'].extend ({'timestamps': timestamps})            
+                            
+                    tr_ref = tr_header
                     
 if __name__ == "__main__":
     main()
