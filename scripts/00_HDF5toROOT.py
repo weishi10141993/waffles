05fdaefdf9a0a064ff1e9daa6b79adab76315065
Renan De Aguiar <rdeaguia@lxplus982.cern.ch>
2024-05-23 17:09:16 +0200

    Added threshold and baseline for the self trigger root file, added some info on the metadata and changed the full stream root

--- scripts/00_HDF5toROOT.py
+++ scripts/00_HDF5toROOT.py
@@ -3,12 +3,16 @@ from hdf5libs import HDF5RawDataFile
 # import daqdataformats
 from daqdataformats import FragmentType
 from rawdatautils.unpack.daphne import *
+from rawdatautils.unpack.utils  import *
+
 import detdataformats
+import fddetdataformats
 
 import os, click, subprocess, stat
 import numpy as np
 from rich.progress import track
 from uproot import recreate as rc
+from uproot.models.TString import Model_TString as TString
 
 def find_endpoint(map_id, target_value):
     for key, value_list in map_id.items():
@@ -16,21 +20,28 @@ def find_endpoint(map_id, target_value):
             return key
 
 def extract_fragment_info(frag):
-    frag_id = str(frag).split(' ')[3][:-1]
-    fragType = frag.get_header().fragment_type
+    frag_id   = str(frag).split(' ')[3][:-1]
+    fragType  = frag.get_header().fragment_type
+    threshold = None
+    baseline  = None
     
     if fragType == FragmentType.kDAPHNE.value:  # For self trigger
-        trigger = 'self_trigger'
-        timestamps = np_array_timestamp(frag)
-        adcs = np_array_adc(frag)
-        channels = np_array_channels(frag)
+        trigger        = 'self_trigger'
+        frame_obj      = fddetdataformats.DAPHNEFrame
+        n_frames       = get_n_frames(frag)
+        daphne_headers = [frame_obj(frag.get_data(iframe*frame_obj.sizeof())).get_header() for iframe in range(n_frames)]
+        threshold      = [header.threshold for header in daphne_headers]
+        baseline       = [header.baseline for header in daphne_headers]
+        timestamps     = np_array_timestamp(frag)
+        adcs           = np_array_adc(frag)
+        channels       = np_array_channels(frag)
     elif fragType == 13:  # For full_stream
         trigger = 'full_stream'
         timestamps = np_array_timestamp_stream(frag)
         adcs = np_array_adc_stream(frag)
         channels = np_array_channels_stream(frag)[0]
     
-    return trigger, frag_id, channels, adcs, timestamps
+    return trigger, frag_id, channels, adcs, timestamps, threshold, baseline
 
 @click.command()
 @click.option("--path", '-p', default = '/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles', help="Insert the run number, ex: 026102")
@@ -62,7 +73,7 @@ def main(path, run, debug):
         path_root = f'{path}/2_daq_root/run_{run}'
         try: 
             os.mkdir(path_root)
-            # os.chmod(f'{path_root}', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+            os.chmod(f'{path_root}', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
         except FileExistsError: print("DATA STRUCTURE ALREADY EXISTS") 
 
         with open(f'{run_path}', "r") as run_list: run_paths = run_list.readlines()
@@ -70,19 +81,18 @@ def main(path, run, debug):
         
         for raw_file in files:
             h5_file   = HDF5RawDataFile(raw_file)
+            run_date  = h5_file.get_attribute('creation_timestamp')
             run_id    = raw_file.split('_')[3]
             root_file = rc(f'{path_root}/{run}_{run_id}.root')
             records   = h5_file.get_all_record_ids()
             inittime  = None
-            #maybe we want to save the thresholds also [ASK DAQ to create np_array_thresholds?]
-            root_file[f'metadata'] = ({'run': (int(run),), 'nrecords': (len(records),), 'det': (det,)})
             
             # Iterate through records 
             for r in track(records, description=f'Dumping {raw_file.split("/")[-1]} ...'):     
                 pds_geo_ids = list(h5_file.get_geo_ids_for_subdetector(r, detdataformats.DetID.string_to_subdetector(det)))
                 
                 trigger_header = h5_file.get_trh(r)
-                trigger_ts = trigger_header.get_header().trigger_timestamp
+                trigger_ts     = trigger_header.get_header().trigger_timestamp
                 
                 # Filtering data with different timestamp on the header
                 # if inittime == trigger_ts: continue 
@@ -102,7 +112,7 @@ def main(path, run, debug):
                 for gid in pds_geo_ids:
 
                     frag = h5_file.get_frag(r, gid)
-                    trigger, frag_id, channels, adcs, timestamps = extract_fragment_info(frag)
+                    trigger, frag_id, channels, adcs, timestamps, threshold, baseline = extract_fragment_info(frag)
                             
                     scr_id   = frag.get_header().element_id.id
                     endpoint = int(find_endpoint(map_id, scr_id))
@@ -115,22 +125,29 @@ def main(path, run, debug):
                         print("CH: ", channels)
                         print("ADCS: ",adcs)
 
-                    if trigger == 'full_stream': adcs = adcs.transpose()
-
+                    data = {
+                        'channel': channels,
+                        'adcs': adcs if trigger == 'self_trigger' else adcs.transpose(),
+                        'timestamps': timestamps,
+                        'repeated_candidate': np.ones(len(channels)) * repeated_candidate
+                    }
+                    
+                    if trigger == 'self_trigger':
+                        data['threshold'] = threshold
+                        data['baseline']  = baseline
+                    
                     if inittime is None:
-                        root_file[f'raw_waveforms'] = ({'channel': channels,
-                                                        'adcs': np.array(adcs),
-                                                        'timestamps': timestamps,
-                                                        'repeated_candidate': np.ones(len(channels))*repeated_candidate})
-                        
+                        root_file['raw_waveforms'] = data
+                        if trigger == 'full_stream':
+                            root_file['timestamp'] = {'timestamps': timestamps}
                     else:
-                        root_file[f'raw_waveforms'].extend({'channel': channels,
-                                                            'adcs': np.array(adcs),
-                                                            'timestamps': timestamps,
-                                                            'repeated_candidate': np.ones(len(channels))*repeated_candidate})
-
+                        root_file['raw_waveforms'].extend(data)
+                        if trigger == 'full_stream':
+                            root_file['timestamp'].extend({'timestamps': timestamps})
+                    
                     inittime = trigger_ts
                     
-
+            root_file[f'metadata'] = ({'run': (int(run),), 'nrecords': (len(records),), 'detector': (TString(det),), 'trigger': (TString(trigger),), 'date' : (TString(run_date),) })
+        
 if __name__ == "__main__":
     main()
