7ac6a21294702633ade4471f506bb4f09433d037
Renan De Aguiar <rdeaguia@lxplus9107.cern.ch>
2024-05-29 17:57:44 +0200

    Few changes on the full strea, filter and root file

--- scripts/00_HDF5toROOT.py
+++ scripts/00_HDF5toROOT.py
@@ -7,6 +7,7 @@ from rawdatautils.unpack.utils  import *
 
 import detdataformats
 import fddetdataformats
+import trgdataformats
 
 import os, click, subprocess, stat
 import numpy as np
@@ -20,18 +21,21 @@ def find_endpoint(map_id, target_value):
             return key
 
 def extract_fragment_info(frag):
+
+    # waveforms
     frag_id   = str(frag).split(' ')[3][:-1]
-    fragType  = frag.get_header().fragment_type
+    frh       = frag.get_header()
+    fragType  = frh.fragment_type
+    scr_id    = frag.get_header().element_id.id
     threshold = None
     baseline  = None
     
     if fragType == FragmentType.kDAPHNE.value:  # For self trigger
         trigger        = 'self_trigger'
         frame_obj      = fddetdataformats.DAPHNEFrame
-        n_frames       = get_n_frames(frag)
-        daphne_headers = [frame_obj(frag.get_data(iframe*frame_obj.sizeof())).get_header() for iframe in range(n_frames)]
-        threshold      = [header.threshold for header in daphne_headers]
-        baseline       = [header.baseline for header in daphne_headers]
+        daphne_headers = [frame_obj(frag.get_data(iframe*frame_obj.sizeof())).get_header() for iframe in range(get_n_frames(frag))]
+        threshold      = np.array([header.threshold for header in daphne_headers])
+        baseline       = np.array([header.baseline for header in daphne_headers])
         timestamps     = np_array_timestamp(frag)
         adcs           = np_array_adc(frag)
         channels       = np_array_channels(frag)
@@ -40,8 +44,18 @@ def extract_fragment_info(frag):
         timestamps = np_array_timestamp_stream(frag)
         adcs = np_array_adc_stream(frag)
         channels = np_array_channels_stream(frag)[0]
+
+    # trigger primitive
+    trg_obj    = trgdataformats.TriggerPrimitive
+    trg_header = [trg_obj(frag.get_data(i_tp*trg_obj.sizeof())) for i_tp in range(int(frag.get_data_size()/trg_obj.sizeof()))]
+        
+    tp_data = [(tp.time_start, tp.time_peak, tp.time_over_threshold, tp.adc_integral, tp.adc_peak, tp.channel, tp.type, tp.flag) for tp in trg_header if tp.type.value == 2]
+    if tp_data:
+        time_start, time_peak, time_over_threshold, adc_integral, adc_peak = zip(*filtered_data)
+    else:
+        time_start = time_peak = time_over_threshold = adc_integral = adc_peak = []
     
-    return trigger, frag_id, channels, adcs, timestamps, threshold, baseline
+    return trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, time_start, time_peak, time_over_threshold, adc_integral, adc_peak
 
 @click.command()
 @click.option("--path", '-p', default = '/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles', help="Insert the run number, ex: 026102")
@@ -85,21 +99,11 @@ def main(path, run, debug):
             run_id    = raw_file.split('_')[3]
             root_file = rc(f'{path_root}/{run}_{run_id}.root')
             records   = h5_file.get_all_record_ids()
-            inittime  = None
+            started   = None
             
             # Iterate through records 
             for r in track(records, description=f'Dumping {raw_file.split("/")[-1]} ...'):     
                 pds_geo_ids = list(h5_file.get_geo_ids_for_subdetector(r, detdataformats.DetID.string_to_subdetector(det)))
-                
-                trigger_header = h5_file.get_trh(r)
-                trigger_ts     = trigger_header.get_header().trigger_timestamp
-                
-                # Filtering data with different timestamp on the header
-                # if inittime == trigger_ts: continue 
-                
-                # Save variable to filter repeated data
-                if inittime == trigger_ts: repeated_candidate = True
-                else: repeated_candidate = False
                 
                 ##TODO: store repeated data in log file?
                 ##TODO: check if the repeated nrecord has only the first frag repeated or all of them
@@ -112,9 +116,8 @@ def main(path, run, debug):
                 for gid in pds_geo_ids:
 
                     frag = h5_file.get_frag(r, gid)
-                    trigger, frag_id, channels, adcs, timestamps, threshold, baseline = extract_fragment_info(frag)
+                    trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, time_start, time_peak, time_over_threshold, adc_integral, adc_peak = extract_fragment_info(frag)
                             
-                    scr_id   = frag.get_header().element_id.id
                     endpoint = int(find_endpoint(map_id, scr_id))
                     channels = 100*int(endpoint) + channels 
 
@@ -126,28 +129,37 @@ def main(path, run, debug):
                         print("ADCS: ",adcs)
 
                     data = {
-                        'channel': channels,
-                        'adcs': adcs if trigger == 'self_trigger' else adcs.transpose(),
-                        'timestamps': timestamps,
-                        'repeated_candidate': np.ones(len(channels)) * repeated_candidate
+                        'channel'   : channels,
+                        'adcs'      : adcs if trigger == 'self_trigger' else adcs.transpose(),
+                        'timestamps': timestamps if trigger == 'self_trigger' else [[timestamps[0]]]*len(channels)
+                    }
+
+                    data_tp ={
+                        'time_start'          : time_start,
+                        'time_peak'           : time_peak,
+                        'time_over_threshold' : time_over_threshold,
+                        'adc_integral'        : adc_integral,
+                        'adc_peak'            : adc_peak
                     }
                     
                     if trigger == 'self_trigger':
-                        data['threshold'] = threshold
-                        data['baseline']  = baseline
-                    
-                    if inittime is None:
+                        data['threshold']  = threshold
+                        data['baseline']   = baseline
+                        data['timestamps'] = timestamps
+
+                    if started is None:
                         root_file['raw_waveforms'] = data
-                        if trigger == 'full_stream':
-                            root_file['timestamp'] = {'timestamps': timestamps}
+                        root_file['trigger_primitives'] = data_tp
+
                     else:
-                        root_file['raw_waveforms'].extend(data)
-                        if trigger == 'full_stream':
-                            root_file['timestamp'].extend({'timestamps': timestamps})
+                        try:
+                            root_file['raw_waveforms'].extend(data)
+                            root_file['trigger_primitives'].extend(data_tp)
+                        except:
+                            print(f'Warning: It was not possible to store the data from record {r} and fragment {frag_id} on the root file.')
+
+                    started = 'yes'
                     
-                    inittime = trigger_ts
-                    
-            root_file[f'metadata'] = ({'run': (int(run),), 'nrecords': (len(records),), 'detector': (TString(det),), 'trigger': (TString(trigger),), 'date' : (TString(run_date),) })
-        
+            root_file['metadata'] = ({'run': (int(run),), 'nrecords': (len(records),), 'detector': (TString(det),), 'trigger': (TString(trigger),), 'date' : (TString(run_date),), 'ticks_to_nanoseconds': (float(1/16),), 'adc_to_volts': (float((1.5*3.2)/(2^(14)-1)),) })
 if __name__ == "__main__":
     main()
