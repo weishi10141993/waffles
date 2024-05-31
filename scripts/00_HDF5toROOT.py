4d6174410b22696947ac47ba39b9a08558a1711b
Renan De Aguiar <rdeaguia@lxplus978.cern.ch>
2024-05-31 17:39:14 +0200

    fixes on root file

--- scripts/00_HDF5toROOT.py
+++ scripts/00_HDF5toROOT.py
@@ -7,7 +7,6 @@ from rawdatautils.unpack.utils  import *
 
 import detdataformats
 import fddetdataformats
-import trgdataformats
 
 import os, click, subprocess, stat
 import numpy as np
@@ -16,6 +15,11 @@ from rich.progress import track
 from uproot import recreate as rc
 from uproot import update as up
 from uproot.models.TString import Model_TString as TString
+
+def find_endpoint(map_id, target_value):
+    for key, value_list in map_id.items():
+        if target_value in value_list:
+            return key
 
 def extract_fragment_info(frag):
 
@@ -26,13 +30,16 @@ def extract_fragment_info(frag):
     scr_id    = frag.get_header().element_id.id
     threshold = None
     baseline  = None
+    trigger_sample_value = None
     
     if fragType == FragmentType.kDAPHNE.value:  # For self trigger
-        trigger        = 'self_trigger'
-        frame_obj      = fddetdataformats.DAPHNEFrame
-        daphne_headers = [frame_obj(frag.get_data(iframe*frame_obj.sizeof())).get_header() for iframe in range(get_n_frames(frag))]
-        threshold      = np.array([header.threshold for header in daphne_headers])
-        baseline       = np.array([header.baseline for header in daphne_headers])
+        trigger              = 'self_trigger'
+        frame_obj            = fddetdataformats.DAPHNEFrame
+        daphne_headers       = [frame_obj(frag.get_data(iframe*frame_obj.sizeof())).get_header() for iframe in range(get_n_frames(frag))]
+        threshold            = np.array([header.threshold for header in daphne_headers])
+        baseline             = np.array([header.baseline for header in daphne_headers])
+        trigger_sample_value = np.array([header.trigger_sample_value for header in daphne_headers])
+
         timestamps     = np_array_timestamp(frag)
         adcs           = np_array_adc(frag)
         channels       = np_array_channels(frag)
@@ -41,24 +48,8 @@ def extract_fragment_info(frag):
         timestamps = np_array_timestamp_stream(frag)
         adcs = np_array_adc_stream(frag)
         channels = np_array_channels_stream(frag)[0]
-
-    # trigger primitive
-    trg_obj    = trgdataformats.TriggerPrimitive
-    trg_header = [trg_obj(frag.get_data(i_tp*trg_obj.sizeof())) for i_tp in range(int(frag.get_data_size()/trg_obj.sizeof()))]
-        
-    tp_data = [(tp.time_start, tp.time_peak, tp.time_over_threshold, tp.adc_integral, tp.adc_peak, tp.channel, tp.type, tp.flag) for tp in trg_header if tp.type.value == 2]
-    
-    if tp_data:
-        time_start, time_peak, time_over_threshold, adc_integral, adc_peak = zip(*filtered_data)
-    else:
-        time_start = time_peak = time_over_threshold = adc_integral = adc_peak = []
-    
-    return trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, time_start, time_peak, time_over_threshold, adc_integral, adc_peak
-
-def to_awkward_array(data):
-    if not isinstance(data, ak.Array):
-        return ak.Array(data)
-    return data
+       
+    return trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, trigger_sample_value
 
 @click.command()
 @click.option("--path", '-p', default = '/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles', help="Insert the run number, ex: 026102")
@@ -121,7 +112,7 @@ def main(path, run, debug):
                 for gid in pds_geo_ids:
 
                     frag = h5_file.get_frag(r, gid)
-                    trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, time_start, time_peak, time_over_threshold, adc_integral, adc_peak = extract_fragment_info(frag)
+                    trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, trigger_sample_value = extract_fragment_info(frag)
                             
                     endpoint = int(find_endpoint(map_id, scr_id))
                     channels = 100*int(endpoint) + channels 
@@ -135,40 +126,25 @@ def main(path, run, debug):
 
                     data = {
                         'channel'   : channels,
-                        'adcs'      : adcs if trigger == 'self_trigger' else adcs.transpose(),
                         'timestamps': timestamps if trigger == 'self_trigger' else [[timestamps[0]]]*len(channels)
                     }
 
-                    data_tp ={
-                        'time_start'          : time_start,
-                        'time_peak'           : time_peak,
-                        'time_over_threshold' : time_over_threshold,
-                        'adc_integral'        : adc_integral,
-                        'adc_peak'            : adc_peak
-                    }
-                    
                     if trigger == 'self_trigger':
+                        data['adcs']                 = adcs
+                        data['threshold']            = threshold
+                        data['baseline']             = baseline
+                        data['trigger_sample_value'] = trigger_sample_value
                         
-                        data['threshold']  = threshold
-                        data['baseline']   = baseline
-                        data['timestamps'] = timestamps
-
-                        adc_size = len(adcs)
-                        started = started_st 
-
-                    else: 
-                        adc_size = len(adcs.transpose())
-                        started = started_fs
+                    else:
+                        adcs         = adcs.transpose()
+                        adcs         = [selected_adcs[:262100] for selected_adcs in adcs]
+                        data['adcs'] = adcs
 
                     try:
-                        root_file[f'{trigger}/raw_waveforms_{adc_size}']      = data
-                        root_file[f'{trigger}/trigger_primitives_{adc_size}'] = data_tp'
- 
+                        root_file[f'raw_waveforms_{trigger}'].extend(data)
                     except:
-                        root_file[f'{trigger}/raw_waveforms_{adc_size}'].extend(data)
-                        root_file[f'{trigger}/trigger_primitives_{adc_size}'].extend(data_tp)
+                        root_file[f'raw_waveforms_{trigger}'] = data
                        
-                    
             root_file['metadata'] = ({'run': (int(run),), 'nrecords': (len(records),), 'detector': (TString(det),), 'date' : (TString(run_date),), 'ticks_to_nanoseconds': (float(1/16),), 'adc_to_volts': (float((1.5*3.2)/(2^(14)-1)),) })
 if __name__ == "__main__":
     main()
