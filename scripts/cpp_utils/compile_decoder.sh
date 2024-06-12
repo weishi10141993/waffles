7c5a9a509f2720ae07da4a8bf1b2ba87d136af4d
LauPM <laurapermol@gmail.com>
2024-06-12 12:31:31 +0200

    change folder name to cpp_utils and added the macros+ script for compilation + README

--- scripts/cpp_utils/compile_decoder.sh
+++ scripts/cpp_utils/compile_decoder.sh
@@ -16,12 +16,12 @@ waffles_path=$(realpath ${BASH_SOURCE[0]})
 #get everything before the last '/'
 waffles_path=$(echo $waffles_path | sed 's|\(.*\)/.*|\1|')
 
-echo -e "\e[35mCloning hdf5libs repo in your env sourcecode folder: \n \e[0m"
-git clone https://github.com/DUNE-DAQ/hdf5libs.git $DBT_AREA_ROOT/sourcecode/hdf5libs
+# echo -e "\e[35mCloning hdf5libs repo in your env sourcecode folder: \n \e[0m"
+# git clone https://github.com/DUNE-DAQ/hdf5libs.git $DBT_AREA_ROOT/sourcecode/hdf5libs
 
-echo -e "\e[35m Compiling hdf5libs (1): \n \e[0m"
-cd $DBT_AREA_ROOT
-dbt-build --clean
+# echo -e "\e[35m Compiling hdf5libs (1): \n \e[0m"
+# cd $DBT_AREA_ROOT
+# dbt-build --clean
 
 
 echo -e "\e[35mCopying the decoder and CMakeLists.txt in the correct folders... \n \e[0m"
@@ -30,7 +30,7 @@ cp $waffles_path/HDF5LIBS_duplications.cpp $DBT_AREA_ROOT/sourcecode/hdf5libs/te
 cp $waffles_path/CMakeLists.txt $DBT_AREA_ROOT/sourcecode/hdf5libs/
 
 echo -e "\e[35mCompiling the added decoder (2): \n \e[0m"
-# cd $DBT_AREA_ROOT
+cd $DBT_AREA_ROOT
 dbt-build --clean
 
 echo -e "\e[32m\nFor using the decoder just type HDF5LIBS_DumptoROOT_filtered <input_file_name> <channel_map_file>\e[0m"
