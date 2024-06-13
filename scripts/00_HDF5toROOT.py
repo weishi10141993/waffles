094fbfba3d6cbfbc3f8c4607ad3bac7b8130b3c5
Renan De Aguiar <rdeaguia@lxplus945.cern.ch>
2024-06-13 11:32:15 +0200

    Fixed progress bar bug, fixed the root structure, repleaced uproot to pyRoot

--- scripts/00_HDF5toROOT.py
+++ scripts/00_HDF5toROOT.py
@@ -10,106 +10,182 @@ import fddetdataformats
 
 import os, click, subprocess, stat
 import numpy as np
+from array import array
 
 from tqdm import tqdm
-from uproot import recreate as rc
-from uproot.models.TString import Model_TString as TString
-from multiprocessing import Pool
+from ROOT import TFile, TTree, std
+from multiprocessing import Pool, current_process, cpu_count
 
-map_id     = {'104': [1, 2, 3, 4], '105': [5, 6, 7, 8], '107': [9, 10], '109': [11], '111': [12], '112': [13], '113': [14]}
+map_id     = {'104': [1, 2, 3, 4], '105': [5, 6, 7, 9], '107': [10, 8], '109': [11], '111': [12], '112': [13], '113': [14]}
 def find_endpoint(map_id, target_value):
     for key, value_list in map_id.items():
         if target_value in value_list:
             return key
 
-def extract_fragment_info(frag):
+def split_list(original_list, n_splits):
+    avg = len(original_list) / float(n_splits)
+    out = []
+    last = 0.0
 
-    # waveforms
-    frag_id   = str(frag).split(' ')[3][:-1]
-    frh       = frag.get_header()
-    fragType  = frh.fragment_type
-    scr_id    = frag.get_header().element_id.id
-    threshold = None
-    baseline  = None
-    trigger_sample_value = None
+    while last < len(original_list):
+        out.append(original_list[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+def extract_fragment_info(frag, trig):
+    frag_id = str(frag).split(' ')[3][:-1]
+    frh     = frag.get_header()
+    trh     = trig.get_header()
+    
+    scr_id           = frh.element_id.id
+    fragType         = frh.fragment_type
+    window_begin_dts = frh.window_begin
+    
+    trigger_timestamp = trh.trigger_timestamp
+    daq_pretrigger    = window_begin_dts - trigger_timestamp
+    
+    threshold = -1
+    baseline  = -1
+    trigger_sample_value = -1
     
     if fragType == FragmentType.kDAPHNE.value:  # For self trigger
         trigger              = 'self_trigger'
         frame_obj            = fddetdataformats.DAPHNEFrame
         daphne_headers       = [frame_obj(frag.get_data(iframe*frame_obj.sizeof())).get_header() for iframe in range(get_n_frames(frag))]
-        threshold            = np.array([header.threshold for header in daphne_headers])
-        baseline             = np.array([header.baseline for header in daphne_headers])
-        trigger_sample_value = np.array([header.trigger_sample_value for header in daphne_headers])
+        threshold            = [header.threshold for header in daphne_headers]
+        baseline             = [header.baseline for header in daphne_headers]
+        trigger_sample_value = [header.trigger_sample_value for header in daphne_headers]
 
-        timestamps     = np_array_timestamp(frag)
-        adcs           = np_array_adc(frag)
-        channels       = np_array_channels(frag)
+        timestamps = np_array_timestamp(frag)
+        adcs       = np_array_adc(frag)
+        channels   = np_array_channels(frag)
+        
     elif fragType == 13:  # For full_stream
-        trigger = 'full_stream'
+        trigger    = 'full_stream'
         timestamps = np_array_timestamp_stream(frag)
-        adcs = np_array_adc_stream(frag)
-        channels = np_array_channels_stream(frag)[0]
+        adcs       = np_array_adc_stream(frag)
+        channels   = np_array_channels_stream(frag)[0]
        
-    return trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, trigger_sample_value
+    return trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, trigger_sample_value, daq_pretrigger
 
 def root_creator(inputs):
     raw_file, path_root, run, debug = inputs
-                
-    det        = 'HD_PDS'
-    h5_file    = HDF5RawDataFile(raw_file)
-    run_date   = h5_file.get_attribute('creation_timestamp')
-    run_id     = raw_file.split('_')[3]
-    root_file  = rc(f'{path_root}/{run}_{run_id}.root')
-    records    = h5_file.get_all_record_ids()
 
-    # Iterate through records
-    #for r in records:
-    for r in tqdm(records):
+    det      = 'HD_PDS'
+    h5_file  = HDF5RawDataFile(raw_file)
+    run_date = h5_file.get_attribute('creation_timestamp')
+    run_id   = raw_file.split('_')[3]
+    run_flow = raw_file.split('_')[4]
+    
+    root_file = TFile( f'{path_root}/{run}_{run_id}_{run_flow}.root' , 'RECREATE')
+
+    fWaveformTree = TTree("raw_waveforms", "raw_waveforms")
+
+    adcs                 = std.vector('float')()
+    timestamps           = array('l', [0])
+    channel              = array('i', [0])
+    baseline             = array('i', [0])
+    trigger_sample_value = array('i', [0])
+    is_fullstream        = array('b', [0])
+    daq_timestamp        = array('l', [0])
+    record               = array('i', [0])
+    
+    fWaveformTree.Branch("record", record, "record/I")
+    fWaveformTree.Branch("daq_timestamp", daq_timestamp, "daq_timestamp/L")
+    fWaveformTree.Branch("adcs", adcs)#, "adcs[{}]/F".format(len(adcs)))
+    fWaveformTree.Branch("timestamps", timestamps, "timestamps/L")
+    fWaveformTree.Branch("channel", channel, "channel/I")
+    fWaveformTree.Branch("baseline", baseline, "baseline/I")
+    fWaveformTree.Branch("trigger_sample_value", trigger_sample_value, "trigger_sample_value/I")
+    fWaveformTree.Branch("is_fullstream", is_fullstream, "is_fullstream/O")
+
+    fMetaDataTree = TTree("metadata", "metadata")
+
+    edp           = std.string()
+    threshold     = std.string()
+    run_          = std.string()
+    nrecords      = array('i', [0])
+    date          = std.string()
+    ticks_to_nsec = array('f', [0])
+    adc_to_volts  = array('f',[0])
+
+    fMetaDataTree.Branch("endpoint", edp)
+    fMetaDataTree.Branch("threshold", threshold)
+    fMetaDataTree.Branch("run", run_)
+    fMetaDataTree.Branch("nrecords", nrecords, "nrecords/I")
+    fMetaDataTree.Branch("date", date, "date/I")
+    fMetaDataTree.Branch("ticks_to_nsec", ticks_to_nsec, "ticks_to_nsec/F")
+    fMetaDataTree.Branch("adc_to_volts", adc_to_volts, "adc_to_volts/F")
+    
+    active_endpoints = set()
+    threshold_list   = []
+
+    records = h5_file.get_all_record_ids()
+    current = current_process()
+    for r in tqdm(records, position = current._identity[0]-1, desc = f'{run}_{run_id}_{run_flow}'):
         pds_geo_ids = list(h5_file.get_geo_ids_for_subdetector(r, detdataformats.DetID.string_to_subdetector(det)))
-
-        if debug: 
-            print("\nTrigger Timestamp from header:", trigger_ts)
-            print("NRECORD: ", r)
         
-        # Iterate through geo_ids
         for gid in pds_geo_ids:
-
             frag = h5_file.get_frag(r, gid)
-            trigger, frag_id, scr_id, channels, adcs, timestamps, threshold, baseline, trigger_sample_value = extract_fragment_info(frag)
+            trig = h5_file.get_trh(r)
+            
+            trigger, frag_id, scr_id, channels_frag, adcs_frag, timestamps_frag, threshold_frag, baseline_frag, trigger_sample_value_frag, daq_pretrigger_frag = extract_fragment_info(frag, trig)
                     
             endpoint = int(find_endpoint(map_id, scr_id))
-            channels = 100*int(endpoint) + channels 
-
+            channels_frag = 100 * int(endpoint) + channels_frag 
+            
             if debug: 
-                print("GEO ID: ", gid)
-                print("FRAG ID: ", frag_id)
-                print("EP: ", endpoint)
-                print("CH: ", channels)
-                print("ADCS: ",adcs)
+                print("GEO ID:", gid)
+                print("FRAG ID:", frag_id)
+                print("EP:", endpoint)
+                print("CH:", channels)
+                print("ADCS:", adcs)
 
-            data = {
-                'channel'   : channels,
-                'timestamps': timestamps if trigger == 'self_trigger' else [[timestamps[0]]]*len(channels)
-            }
-
-            if trigger == 'self_trigger':
-                data['adcs']                 = adcs
-                data['threshold']            = threshold
-                data['baseline']             = baseline
-                data['trigger_sample_value'] = trigger_sample_value
+            if trigger == 'full_stream':
+                adcs_frag                 = adcs_frag.transpose()
+                timestamps_frag           = [timestamps_frag[0]] * len(channels_frag)
+                baseline_frag             = [-1] * len(channels_frag)
+                trigger_sample_value_frag = [-1] * len(channels_frag)
+                is_fullstream_frag        = [True] * len(channels_frag)
+            elif trigger == 'self_trigger':
+                is_fullstream_frag = [False] * len(channels_frag)
+            
+            if endpoint not in active_endpoints: 
+                active_endpoints.add(endpoint)
+                threshold_list.append(threshold_frag)
                 
-            else:
-                adcs         = adcs.transpose()
-                adcs         = [selected_adcs[:262100] for selected_adcs in adcs]
-                data['adcs'] = adcs
+            for index, ch in enumerate(channels_frag):
 
-            try:
-                root_file[f'raw_waveforms_{trigger}'].extend(data)
-            except:
-                root_file[f'raw_waveforms_{trigger}'] = data
-               
-    root_file['metadata'] = ({'run': (int(run),), 'nrecords': (len(records),), 'detector': (TString(det),), 'date' : (TString(run_date),), 'ticks_to_nanoseconds': (float(1/16),), 'adc_to_volts': (float((1.5*3.2)/(2^(14)-1)),) })
+                adcs.clear()
+                for value in adcs_frag[index]:
+                    adcs.push_back(value)
+                timestamps[0]           = timestamps_frag[index]
+                channel[0]              = ch
+                baseline[0]             = baseline_frag[index]
+                trigger_sample_value[0] = trigger_sample_value_frag[index]
+                is_fullstream[0]        = is_fullstream_frag[index]
+                daq_timestamp[0]        = daq_pretrigger_frag
+                record[0]               = r[0]
+                fWaveformTree.Fill()
 
+    fWaveformTree.Write("", TFile.kOverwrite)    
+
+    active_endpoints = str(active_endpoints)
+    threshold_list   = str(threshold_list)
+
+    edp.assign(active_endpoints) 
+    threshold.assign(threshold_list)
+    run_.assign(run)
+    nrecords[0]  = len(records)
+    date.assign(run_date)
+    ticks_to_nsec[0] = 1/16
+    adc_to_volts[0]  = (1.5*3.2)/(2^(14)-1)
+    fMetaDataTree.Fill()
+    
+    fMetaDataTree.Write("", TFile.kOverwrite)
+    root_file.Close()
+    
 @click.command()
 @click.option("--path", '-p', default = '/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles', help="Insert the desired path.")
 @click.option("--run" , '-r', default = None, help="Insert the run number, ex: 026102")
@@ -124,6 +200,8 @@ def main(path, run, debug):
         
     if len(run)!=1: runs_list = list(map(int, list(run.split(","))))
     else: runs_list = run
+
+    num_cores_mp = cpu_count()
     
     for run in runs_list:
         run = str(run).zfill(6) # check if run have 6 digits and fill with zeros if not
@@ -143,12 +221,26 @@ def main(path, run, debug):
             os.chmod(f'{path_root}', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
         except FileExistsError: print("DATA STRUCTURE ALREADY EXISTS") 
 
-        with open(f'{run_path}', "r") as run_list: run_paths = run_list.readlines()
-        files  = [run_path.rstrip('\n') for run_path in run_paths]
-        inputs = [[raw_file, path_root, run, debug] for raw_file in files]
+        with open(f'{run_path}', "r") as run_list: 
+            run_paths = run_list.readlines()
+     
+        print(f'Cores avaliable:{num_cores_mp}')
+        print(f'Files to be processed: {len(run_paths)}')
+    
+        if num_cores_mp > len(run_paths): 
+            process  = len(run_paths)
+            nprocess = 1
+        else: 
+            process  = num_cores_mp
+            nprocess = len(run_paths)//num_cores_mp
         
-        with Pool(processes=len(files)) as pool:
-            process = pool.map(root_creator, inputs)
+        files  = [run_path.rstrip('\n') for run_path in run_paths]
+        split_files = split_list(files, nprocess)
+        for p in range(nprocess):
+            print(f'- Process{p}: {split_files[p]}')
+            with Pool(process) as pool:
+                inputs  = [[raw_file, path_root, run, debug] for raw_file in split_files[p]]
+                process = pool.map(root_creator, inputs)
         
 if __name__ == "__main__":
     main()
