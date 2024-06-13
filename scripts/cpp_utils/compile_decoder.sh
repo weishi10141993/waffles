541ccf68a25ffbf4a9219b00c6d9b1198f25f71a
Laura Pérez-Molina <80100549+LauPM@users.noreply.github.com>
2024-06-13 10:33:42 +0200

    Update compile_decoder.sh

--- scripts/cpp_utils/compile_decoder.sh
+++ scripts/cpp_utils/compile_decoder.sh
@@ -13,15 +13,14 @@ echo -e " This process is needed just the first time you want to run this tool "
 echo -e " Enjoy! :) \n \e[0m"
 
 waffles_path=$(realpath ${BASH_SOURCE[0]})
-#get everything before the last '/'
 waffles_path=$(echo $waffles_path | sed 's|\(.*\)/.*|\1|')
 
-# echo -e "\e[35mCloning hdf5libs repo in your env sourcecode folder: \n \e[0m"
-# git clone https://github.com/DUNE-DAQ/hdf5libs.git $DBT_AREA_ROOT/sourcecode/hdf5libs
+echo -e "\e[35mCloning hdf5libs repo in your env sourcecode folder: \n \e[0m"
+git clone https://github.com/DUNE-DAQ/hdf5libs.git $DBT_AREA_ROOT/sourcecode/hdf5libs
 
-# echo -e "\e[35m Compiling hdf5libs (1): \n \e[0m"
-# cd $DBT_AREA_ROOT
-# dbt-build --clean
+echo -e "\e[35m Compiling hdf5libs (1): \n \e[0m"
+cd $DBT_AREA_ROOT
+dbt-build --clean
 
 
 echo -e "\e[35mCopying the decoder and CMakeLists.txt in the correct folders... \n \e[0m"
