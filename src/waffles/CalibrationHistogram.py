433a8416c3ff3350432cf65b47a4c8891e10ce5e
Julio <jurenag@outlook.es>
2024-07-03 15:49:16 +0200

    Add fitting methods at the CalibrationHistogram level

--- src/waffles/CalibrationHistogram.py
+++ src/waffles/CalibrationHistogram.py
@@ -1,5 +1,10 @@
+import math
+
 import numpy as np
-from typing import List, Optional
+from scipy import signal as spsi
+from scipy import optimize as spopt
+from plotly import graph_objects as pgo
+from typing import Tuple, List, Dict, Optional
 
 from .WaveformSet import WaveformSet
 from .Exceptions import generate_exception_message
@@ -359,6 +364,329 @@ class CalibrationHistogram:
         """
     
         return scale * np.exp( -1. * (np.power( ( x - mean ) / ( 2 * std ), 2)))
+    
+    def __spot_first_peaks(self,    max_peaks : int,
+                                    prominence : float,
+                                    initial_percentage = 0.1,
+                                    percentage_step = 0.1) -> Tuple[bool, Tuple]:
+        
+        """
+        This method is not intended for user usage. It must
+        be only called by CalibrationHistogram.fit_peaks(),
+        where the well-formedness checks of the input
+        parameters have been performed. This method tries 
+        to find peaks over the signal which is computed as
+
+            signal = (self.Counts - np.min(self.Counts))/np.max(self.Counts)
+
+        This method iteratively calls
+
+            scipy.signal.find_peaks(signal[0:points], 
+                                    prominence = prominence)
+
+        to spot, at most, max_peaks peaks. To do so, at the 
+        first iteration, points is computed as 
+        math.floor(initial_percentage * len(signal)). If the 
+        number of spotted peaks is less than max_peaks, then 
+        points is increased by 
+        math.floor(percentage_step * len(signal)) and the 
+        scipy.signal.find_peaks() method is called again. This 
+        process is repeated until the number of spotted peaks
+        is equal to max_peaks, or until the number of points 
+        reaches len(signal). If the number of points reaches 
+        len(signal), then scipy.signal.find_peaks() is called 
+        one last time as
+
+            scipy.signal.find_peaks(signal, 
+                                    prominence = prominence)
+        
+        If the last call found a number of peaks smaller than
+        max_peaks, then this function returns (False, peaks),
+        where peaks is the output of the last call to 
+        scipy.signal.find_peaks(). If the last call found a
+        number of peaks greater than or equal to max_peaks, 
+        then the function returns (True, peaks), where peaks 
+        is the output of scipy.signal.find_peaks() but 
+        truncated to the first max_peaks found peaks.
+
+        Parameters
+        ----------
+        max_peaks : int
+            The maximum number of peaks to spot. It must 
+            be a positive integer. This is not checked here, 
+            it is the caller's responsibility to ensure this.
+        prominence : float
+            The prominence parameter to pass to the
+            scipy.signal.find_peaks() method. Since the
+            signal is normalized, this prominence can be
+            understood as a fraction of the total amplitude
+            of the signal. P.e. setting prominence to 0.5,
+            will prevent scipy.signal.find_peaks() from
+            spotting peaks whose amplitude is less than
+            half of the total amplitude of the signal.
+        initial_percentage : float
+            The initial percentage of the signal to
+            consider. It must be greater than 0.0
+            and smaller than 1.0.
+        percentage_step : float
+            The percentage step to increase the signal
+            to consider in successive calls of 
+            scipy.signal.find_peaks(). It must be greater 
+            than 0.0 and smaller than 1.0.
+
+        Returns
+        -------
+        output : tuple of ( bool and tuple )
+            The first entry is a boolean which is True if 
+            the number of peaks found is greater than or 
+            equal to max_peaks, and False otherwise. The
+            second entry is the output of the last call to
+            scipy.signal.find_peaks() if the first entry
+            of the tuple is False. If the first entry of
+            the tuple is True, then the second entry is
+            the output of scipy.signal.find_peaks() but
+            truncated to the first max_peaks found peaks. 
+            For more information, check the 
+            scipy.signal.find_peaks() documentation.
+        """
+
+        signal = ( self.Counts - np.min(self.Counts) ) / np.max(self.Counts)
+
+        fFoundMax = False
+        fReachedEnd = False
+        points = math.floor( initial_percentage * len(signal) )
+
+        while not fFoundMax and not fReachedEnd:
+
+            points = min(points, len(signal))
+
+            spsi_output = spsi.find_peaks(  signal[0:points],
+                                            prominence = prominence,
+                                            width = 0,                  # Adding a minimal 0 width, which constraints nothing,
+                                            rel_height = 0.5)           # but which makes scipy.signal.find_peaks() return
+                                                                        # information about each peak-width at half its height.
+            if len(spsi_output[0]) >= max_peaks:
+
+                spsi_output = CalibrationHistogram.trim_spsi_find_peaks_output_to_max_peaks(spsi_output,
+                                                                                            max_peaks)
+                fFoundMax = True
+
+            if points == len(signal):
+                fReachedEnd = True
+
+            points += math.floor( percentage_step * len(signal) )
+
+        if fFoundMax:
+            return (True, spsi_output)
+        else:
+            return (False, spsi_output)
+        
+    @staticmethod
+    def trim_spsi_find_peaks_output_to_max_peaks(   spsi_output : Tuple[np.ndarray, Dict[str, np.ndarray]],
+                                                    max_peaks : int) -> Tuple[np.ndarray, Dict[str, np.ndarray]]:
+        
+        """
+        This function gets the output of a certain call to 
+        scipy.signal.find_peaks() and returns the same output, 
+        but truncated to the first max_peaks found peaks. 
+
+        Parameters
+        ----------
+        spsi_output : tuple of ( np.ndarray and dict )
+            The output of a call to scipy.signal.find_peaks().
+            No checks are performed here regarding the
+            well-formedness of this input.
+        max_peaks : int
+            The maximum number of peaks to keep. It must be a
+            positive integer. This is not checked here. It is
+            the caller's responsibility to ensure this. If the 
+            given spsi_output has a number less or equal to 
+            max_peaks peaks, then the output of this function 
+            will be the same as the given spsi_output. If it
+            has more than max_peaks peaks, then the output of
+            this function will contain only the information
+            regarding the first max_peaks peaks of the given 
+            spsi_output.
+
+        Returns
+        -------
+        output : tuple of ( np.ndarray and dict )
+            The first element is an unidimensional numpy array 
+            which contains the first max_peaks elements of the 
+            first element of the given spsi_output. The second 
+            element is a dictionary which contains the same keys 
+            as the second element of the given spsi_output, but 
+            the values (which are unidimensional numpy arrays) 
+            contain only the first max_peaks elements of the 
+            values of the second element of the given spsi_output.
+        """
+
+        if len(spsi_output[0]) <= max_peaks:
+            return spsi_output
+        else:
+            return (spsi_output[0][0:max_peaks], {key: value[0:max_peaks] for key, value in spsi_output[1].items()})
+        
+    def fit_peaks(self, max_peaks : int,
+                        prominence : float,
+                        half_points_to_fit : int,
+                        initial_percentage = 0.1,
+                        percentage_step = 0.1) -> bool:
+            
+        """
+        This method
+            
+            -   tries to find the first max_peaks of 
+                this calibration histogram whose prominence
+                is greater than the given prominence parameter,
+                using the scipy.signal.find_peaks() function 
+                iteratively. This function delegates this
+                task to the 
+                CalibrationHistogram.__spot_first_peaks() 
+                method.
+
+            -   Then, it fits a gaussian function to each one
+                of the found peaks using the output of 
+                the last call to scipy.signal.find_peaks()
+                (which is returned by 
+                CalibrationHistogram.__spot_first_peaks())
+                as a seed for the fit.
+
+            -   Finally, it stores the fit parameters in the
+                GaussianFitsParameters attribute, according
+                to its structure, which can be found in the
+                class documentation.
+
+        This method returns True if the number of found peaks
+        matches the given max_peaks parameter, and False
+        if it is smaller than max_peaks.
+        
+        Parameters
+        ----------
+        max_peaks : int
+            It must be a positive integer. It gives the
+            maximum number of peaks that could be possibly
+            fit. This parameter is passed to the 'max_peaks'
+            parameter of the 
+            CalibrationHistogram.__spot_first_peaks() method.
+        prominence : float
+            It must be greater than 0.0 and smaller than 1.0.
+            It gives the minimal prominence of the peaks to 
+            spot. This parameter is passed to the 'prominence' 
+            parameter of the 
+            CalibrationHistogram.__spot_first_peaks() method,
+            where it is interpreted as the fraction of the 
+            total amplitude of the histogram which is required 
+            for a peak to be spotted as such. P.e. setting 
+            prominence to 0.5, will prevent scipy.signal.find_peaks() 
+            from spotting peaks whose amplitude is less than
+            half of the total amplitude of the histogram.
+        half_points_to_fit : int
+            It must be a positive integer. For each peak, it 
+            gives the number of points to consider on either 
+            side of the peak maximum, to fit each gaussian 
+            function. I.e. if i is the iterator value for
+            self.Counts of the i-th peak, then the histogram
+            bins which will be considered for the fit are
+            given by the slice 
+            self.Counts[i - half_points_to_fit : i + half_points_to_fit + 1].
+        initial_percentage : float
+            It must be greater than 0.0 and smaller than 1.0.
+            This parameter is passed to the 'initial_percentage' 
+            parameter of the 
+            CalibrationHistogram.__spot_first_peaks() method. 
+            For more information, check the documentation of 
+            such method.
+        percentage_step : float
+            It must be greater than 0.0 and smaller than 1.0.
+            This parameter is passed to the 'percentage_step' 
+            parameter of the 
+            CalibrationHistogram.__spot_first_peaks() method. 
+            For more information, check the documentation of 
+            such method.
+
+        Returns
+        -------
+        bool
+            True if the number of found peaks matches the given
+            max_peaks parameter, and False if it is smaller than
+            max_peaks.
+        """
+
+        if max_peaks < 1:
+            raise Exception(generate_exception_message( 1,
+                                                        'CalibrationHistogram.fit_peaks()',
+                                                        f"The given max_peaks ({max_peaks}) must be greater than 0."))
+        if prominence <= 0.0 or prominence >= 1.0:
+            raise Exception(generate_exception_message( 2,
+                                                        'CalibrationHistogram.fit_peaks()',
+                                                        f"The given prominence ({prominence}) must be greater than 0.0 and smaller than 1.0."))
+        
+        if initial_percentage <= 0.0 or initial_percentage >= 1.0:
+            raise Exception(generate_exception_message( 3,
+                                                        'CalibrationHistogram.fit_peaks()',
+                                                        f"The given initial_percentage ({initial_percentage}) must be greater than 0.0 and smaller than 1.0."))
+    
+        if percentage_step <= 0.0 or percentage_step >= 1.0:
+            raise Exception(generate_exception_message( 4,
+                                                        'CalibrationHistogram.fit_peaks()',
+                                                        f"The given percentage_step ({percentage_step}) must be greater than 0.0 and smaller than 1.0."))
+        self.__reset_gaussian_fit_parameters()
+
+        fFoundMax, spsi_output = self.__spot_first_peaks(   max_peaks,
+                                                            prominence,
+                                                            initial_percentage,
+                                                            percentage_step)
+        peaks_n_to_fit = len(spsi_output[0])
+
+        for i in range(peaks_n_to_fit):
+                
+            aux_idx  = spsi_output[0][i]
+
+            aux_seeds = [   self.Counts[aux_idx],                                           # Scale seed
+                            (self.Edges[aux_idx] + self.Edges[aux_idx + 1]) / 2.,           # Mean seed
+                            spsi_output[1]['widths'][i] * self.__mean_bin_width / 2.355]    # Std seed : Note that 
+                                                                                            # CalibrationHistogram.__spot_first_peaks() 
+                                                                                            # is computing the widths of the peaks, in
+                                                                                            # number of samples, at half of their height 
+                                                                                            # (rel_height = 0.5). 2.355 is approximately 
+                                                                                            # the conversion factor between the standard 
+                                                                                            # deviation and the FWHM. Also, note that here
+                                                                                            # we are assuming that the binning is uniform.
+            aux_lower_lim = max(0,
+                                aux_idx - half_points_to_fit)   # Restrict the fit lower limit to 0
+            
+            aux_upper_lim = min(len(self.Counts) - 1,                   # The upper limit should be restricted len(self.Counts).
+                                aux_idx + half_points_to_fit + 1)       # Making it be further restricted to len(self.Counts) - 1
+                                                                        # so that there is always available data to compute the
+                                                                        # center of the bins, in the following line.
+            
+            aux_bin_centers = ( self.Edges[aux_lower_lim : aux_upper_lim] + self.Edges[aux_lower_lim + 1 : aux_upper_lim + 1] ) / 2.
+            aux_counts = self.Counts[aux_lower_lim : aux_upper_lim]
+
+            try:
+                aux_optimal_parameters, aux_covariance_matrix  = spopt.curve_fit(   CalibrationHistogram.gaussian, 
+                                                                                    aux_bin_centers, 
+                                                                                    aux_counts, 
+                                                                                    p0 = aux_seeds)
+            except RuntimeError:    # Happens if scipy.optimize.curve_fit()
+                                    # could not converge to a solution
+        
+                fFoundMax = False   # In this case, we will skip this peak
+                                    # (so, in case fFoundMax was True, now 
+                                    # it must be false) and we will continue 
+                                    # with the next one, if any
+                continue    
+
+            aux_errors = np.sqrt(np.diag(aux_covariance_matrix))
+
+            self.__add_gaussian_fit_parameters( aux_optimal_parameters[0],
+                                                aux_errors[0],
+                                                aux_optimal_parameters[1],
+                                                aux_errors[1],
+                                                aux_optimal_parameters[2],
+                                                aux_errors[2])
+        return fFoundMax
+
     def plot(self,  figure : pgo.Figure,
                     name : Optional[str] = None,
                     row : Optional[int] = None,
