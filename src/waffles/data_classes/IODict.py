89f57e36a5480e2d00491249959a45ef61b0fa83
Julio Ureña <78102932+jurenag@users.noreply.github.com>
2024-09-04 12:53:49 +0200

    Abide by PEP8 in IODict.py

--- src/waffles/data_classes/IODict.py
+++ src/waffles/data_classes/IODict.py
@@ -1,7 +1,5 @@
 class IoDict(dict):
-
-    """
-    Stands for Input/Output Dictionary. This class
+    """Stands for Input/Output Dictionary. This class
     inherits from the built-in dict class. This class
     adds no further members to the built-in dict, except
     for the fact that it re-implements the representation
@@ -9,8 +7,7 @@ class IoDict(dict):
     """
 
     def __repr__(self):
-        """
-        This re-implementation of the __repr__ method
+        """This re-implementation of the __repr__ method
         provides a line-per-item representation of the
         dictionary, showing the key, the type of the
         key, the value and the type of the value for
@@ -19,8 +16,7 @@ class IoDict(dict):
 
         aux = '{  '
         for key in self.keys():
-            aux += f"\t {key} ({type(key)
-                                }) : {self[key]} ({type(self[key])}),\n"
+            aux += f"\t {key} ({type(key)}) : {self[key]} ({type(self[key])}),\n"
         aux = aux[:-2] + '\t}'
 
         return aux
