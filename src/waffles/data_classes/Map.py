e07b1e606a47d1bc7e8cc368812b5f3b27da1b5c
Julio Ureña <78102932+jurenag@users.noreply.github.com>
2024-09-04 12:31:10 +0200

    Abide by PEP8 in Map.py

--- src/waffles/data_classes/Map.py
+++ src/waffles/data_classes/Map.py
@@ -6,9 +6,7 @@ from waffles.Exceptions import GenerateExceptionMessage
 
 
 class Map:
-
-    """
-    This class implements a list of lists which make up a
+    """This class implements a list of lists which make up a
     bi-dimensional non-ragged array which represents a map.
     Each entry of this non-ragged array is an object of a
     certain type, which is homogenous across the whole
@@ -16,13 +14,13 @@ class Map:
 
     Attributes
     ----------
-    Rows : int
+    rows: int
         The number of rows in the map
-    columns : int
+    columns: int
         The number of columns in the map
-    type : type
+    type: type
         The type of the objects stored in the map
-    data : list of lists
+    data: list of lists
         Nested list which contains the data of the
         map. data[i][j] gives the object stored in
         the i-th row and j-th column of the map.
@@ -33,21 +31,21 @@ class Map:
     """
 
     def __init__(
-            self, rows: int,
-            columns: int,
-            type_: type,
-            data: Optional[List[List[Any]]] = None):
-        """
-        Map class initializer
+        self, : int,
+        columns: int,
+        type_: type,
+        data: Optional[List[List[Any]]] = None
+    ):
+        """Map class initializer
 
         Parameters
         ----------
-        rows : int
+        rows: int
             It must be a positive integer
-        columns : int
+        columns: int
             It must be a positive integer
-        type_ : type
-        data : list of lists
+        type_: type
+        data: list of lists
             It must be a list of lists of objects of the
             same type as the one specified in the type_
             parameter. The length of data must be equal to
@@ -62,6 +60,7 @@ class Map:
                 1,
                 'Map.__init__()',
                 f"The given number of rows ({rows}) must be positive."))
+            
         if columns < 1:
             raise Exception(GenerateExceptionMessage(
                 2,
@@ -79,6 +78,7 @@ class Map:
                 4,
                 'Map.__init__()',
                 f"The type of the objects in the given data must be {type_}."))
+            
         self.__rows = rows
         self.__columns = columns
         self.__type = type_
@@ -105,17 +105,17 @@ class Map:
     def list_of_lists_is_well_formed(
             grid: List[List[Any]],
             nrows: int,
-            ncols: int) -> bool:
-        """
-        This method returns True if the given grid contains
+            ncols: int
+    ) -> bool:
+        """This method returns True if the given grid contains
         nrows lists, each of which has a length equal to ncols.
         It returns False if else.
 
         Parameters
         ----------
-        grid : list of lists
-        nrows : int
-        ncols : int
+        grid: list of lists
+        nrows: int
+        ncols: int
 
         Returns
         ----------
@@ -127,6 +127,7 @@ class Map:
                 1,
                 'Map.list_of_lists_is_well_formed()',
                 'The number of rows and columns must be positive.'))
+            
         if len(grid) != nrows:
             return False
         else:
@@ -137,31 +138,31 @@ class Map:
 
     @classmethod
     def from_unique_value(
-            cls,
-            nrows: int,
-            ncols: int,
-            type_: type,
-            value,
-            independent_copies=False) -> 'Map':
-        """
-        This method returns a Map object whose Rows, columns
+        cls,
+        nrows: int,
+        ncols: int,
+        type_: type,
+        value,
+        independent_copies=False
+    ) -> 'Map':
+        """This method returns a Map object whose Rows, columns
         and Type attributes match the input parameters nrows,
         ncols and type_, and for which all of its entries
         are equal to the input value.
 
         Parameters
         ----------
-        nrows (resp. ncols) : int
+        nrows (resp. ncols): int
             Number of rows (resp. columns) of the returned
             Map object. It must be a positive integer.
-        type_ : type
+        type_: type
             Type of the object(s) stored in the returned Map
             object
         value
             Its type must match the type_ parameter. It is
             the value that all the entries of the returned
             Map object will have.
-        independent_copies : bool
+        independent_copies: bool
             If True, the returned Map object will have
             independent copies of the value parameter in each
             one of its entries. If False, the returned Map object
